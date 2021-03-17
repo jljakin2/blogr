93d2f94ab07555669c6175fab80ff081581416e4
Jeff Jakinovich <jeff.jak13@gmail.com>
2021-03-17 09:19:49 -0400

    full desktop version pre media queries

---
+++ index.js
@@ -0,0 +1,17 @@
+// const navItems = document.querySelectorAll(".nav-item");
+
+// navItems.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     const subItem = item.firstElementChild.nextElementSibling;
+
+//     subItem.classList.add("visible");
+//   });
+// });
+
+// navItems.forEach((item) => {
+//   item.addEventListener("mouseleave", () => {
+//     const subItem = item.firstElementChild.nextElementSibling;
+
+//     subItem.classList.remove("visible");
+//   });
+// });
